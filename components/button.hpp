e92b96414fae6bfd19c457375f860a7bc6cb4f3d
callmelucian <luciannguyen07@gmail.com>
2026-03-05 08:49:07 +0700

    Add mouse events for buttons

--- components/button.hpp
+++ components/button.hpp
@@ -2,6 +2,7 @@
 #include <SFML/Graphics.hpp>
 #include <functional>
 #include "../assets/theme.hpp"
+#include <iostream>
 
 const sf::Color idleColor = sf::Color({100, 100, 100});
 
@@ -9,23 +10,23 @@ class Button : public sf::Drawable, public sf::Transformable {
 private:
     sf::RectangleShape rectangle;
     sf::Text label;
-    sf::Color activeColor;
     std::function<void()> onClick;
     bool activation;
 
 public:
-    Button (float width, float height, const std::string &labelString, int fontSize,
-            const sf::Color &activeColor) :
-        label(Theme::ibmRegular, labelString), activeColor(activeColor), activation(true) {
+    Button (float width, float height, const std::string &labelString, float fontSize, float thickness) :
+        label(Theme::notoRegular, labelString), activation(false) {
         
         // setup rectangle
         rectangle.setSize({width, height});
         rectangle.setOrigin({width / 2.f, height / 2.f});
-        rectangle.setFillColor(activeColor);
+        rectangle.setFillColor(sf::Color::White);
+        rectangle.setOutlineThickness(thickness);
+        rectangle.setOutlineColor(sf::Color::Black);
 
         // setup label
         label.setCharacterSize(fontSize);
-        label.setFillColor(sf::Color::White);
+        label.setFillColor(sf::Color::Black);
 
         sf::FloatRect localRectangle = label.getLocalBounds();
         label.setOrigin({
@@ -53,8 +54,25 @@ public:
         return rectangle.getGlobalBounds().contains(localPos);
     }
 
-    void handleClick (const sf::Vector2f &mousePos) {
+    void handleHover (const sf::Vector2f &mousePos) {
+        if (!activation)
+            rectangle.setFillColor(containPosition(mousePos) ? sf::Color({220, 220, 220}) : sf::Color::White);
+    }
+
+    void handleMousePress (const sf::Vector2f &mousePos) {
         if (!containPosition(mousePos)) return;
+        rectangle.setFillColor(sf::Color({200, 200, 200}));
+        rectangle.setScale({0.97f, 0.97f});
+        label.setScale({0.97f, 0.97f});
+        activation = true;
+    }
+
+    void handleMouseRelease (const sf::Vector2f &mousePos) {
+        if (!activation) return;
+        rectangle.setFillColor(containPosition(mousePos) ? sf::Color({220, 220, 220}) : sf::Color::White);
+        rectangle.setScale({1.f, 1.f});
+        label.setScale({1.f, 1.f});
+        activation = false;
         onClick();
     }
 };
--- scenes/linked-list-scene.hpp
+++ scenes/linked-list-scene.hpp
@@ -19,7 +19,7 @@ private:
 
 public:
     LinkedListScene (const sf::RenderWindow &window) :
-        Scene(Theme::getBackground(), window), button(200, 100, "Button", 40, sf::Color({69, 1, 69})),
+        Scene(Theme::getBackground(), window), button(200, 100, "Button", 40, 3),
         actionBar(boundR, actionBarHeight, Theme::getBackground(), sf::Color::Black, "Linked-list") {
             actionBar.setPosition({boundR / 2.f, actionBarHeight / 2.f});
 
@@ -39,16 +39,18 @@ public:
                 std::cerr << "Button clicked" << std::endl;
                 return;
             });
-            button.setPosition({150, 150});
+            button.setPosition({900, 400});
         }
 
     void handleEvent (sf::RenderWindow &window, const std::optional<sf::Event> &event) {
+        // Handle event: Mouse hover
+        button.handleHover(window.mapPixelToCoords(sf::Mouse::getPosition(window)));
         // Handle event: Mouse pressed
         if (const auto *mousePressed = event->getIf<sf::Event::MouseButtonPressed>()) {
             if (mousePressed->button == sf::Mouse::Button::Left) {
                 sf::Vector2f mousePos = window.mapPixelToCoords(mousePressed->position);
                 nodes.handleMouseClick(mousePos);
-                button.handleClick(mousePos);
+                button.handleMousePress(mousePos);
             }
         }
         // Handle event: Mouse moved
@@ -56,11 +58,12 @@ public:
             sf::Vector2f mousePos = window.mapPixelToCoords(mouseMovement->position);
             nodes.handleMouseMovement(mousePos);
         }
-        // Handle event: Mouse released
+        // Handle event: Mouse released (left-click)
         if (const auto *mouseReleased = event->getIf<sf::Event::MouseButtonReleased>()) {
             if (mouseReleased->button == sf::Mouse::Button::Left) {
                 sf::Vector2f mousePos = window.mapPixelToCoords(mouseReleased->position);
                 nodes.handleMouseRelease(mousePos);
+                button.handleMouseRelease(mousePos);
             }
         }
     }
@@ -70,8 +73,8 @@ public:
     }
 
     void draw (sf::RenderWindow &window) {
-        for (auto &node : nodes) window.draw(*node);
-        for (auto &edge : edges) window.draw(edge);
+        // for (auto &node : nodes) window.draw(*node);
+        // for (auto &edge : edges) window.draw(edge);
         window.draw(actionBar);
         window.draw(button);
     }
