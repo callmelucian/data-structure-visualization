a935ad4073cfa7039f295985fa959d2eb7e857aa
callmelucian <luciannguyen07@gmail.com>
2026-03-29 14:01:45 +0700

    Further implementations for animation manager

--- include/animation/animation-event.hpp
+++ include/animation/animation-event.hpp
@@ -13,13 +13,60 @@ template <typename TypeUI>
 class AnimationManager {
 private:
     std::queue<std::unique_ptr<AnimationEvent<TypeUI>>> animationQueue;
+    std::vector<TypeUI> stateUI;
     std::queue<int> eventIDQueue;
-    int currentIndex, displayIndex;
+    int currentEventStep, displayEventStep, currentAction, displayAction, stateIterator;
+    std::function<void(bool)> callbackSetNextState, callbackSetPreviousState;
 
 public:
+    /**
+     * @brief Assign a callback function that will be called
+     * whenever Animation Manager need to enable/disable the next-state button
+     */
+    void setCallbackSetNextState (auto func) {
+        setCallbackSetNextState = func;
+    }
+
+    /**
+     * @brief Assign a callback function that will be called
+     * whenever Animation Manager need to enable/disable the previous-state button
+     */
+    void setCallbackSetPreviousState (auto funct) {
+        setCallbackSetPreviousState = func;
+    }
+
+    /**
+     * @brief constructor
+     */
+    AnimationManager (const TypeUI &init);
+
+    /**
+     * @brief create an animation event with a corresponding event ID
+     */
     void createAnimationEvent (std::unique_ptr<AnimationEvent<TypeUI>> event);
 
+    /**
+     * @brief use next event ID
+     */
     void nextStep();
 
-    void popAnimation (TypeUI &ui);
+    /**
+     * @brief apply all animation steps with the next displaying event ID
+     */
+    void popAnimation();
+
+    /**
+     * @brief get the current UI to draw
+     */
+    TypeUI& getCurrentUI();
+
+    /**
+     * @brief go back to the previous UI state
+     */
+    void previousState();
+
+    /**
+     * @brief pass to the next UI state
+     */
+    void nextState();
 };
--- src/animation/animation-event.cpp
+++ src/animation/animation-event.cpp
@@ -1,22 +1,57 @@
 #include "../../include/animation/animation-event.hpp"
 
 template <typename TypeUI>
+AnimationManager<TypeUI>::AnimationManager (const TypeUI &init) :
+    stateUI(1, init), currentEventStep(0), displayEventStep(0), currentAction(0), displayAction(0), stateIterator(0) {}
+
+template <typename TypeUI>
 void AnimationManager<TypeUI>::createAnimationEvent (std::unique_ptr<AnimationEvent<TypeUI>> event) {
     animationQueue.push(std::move(event));
-    eventIDQueue.push(currentIndex);
+    eventIDQueue.push(currentEventStep);
 }
 
 template <typename TypeUI>
 void AnimationManager<TypeUI>::nextStep() {
-    currentIndex++;
+    currentEventStep++;
 }
 
 template <typename TypeUI>
-void AnimationManager<TypeUI>::popAnimation (TypeUI &ui) {
+void AnimationManager<TypeUI>::popAnimation() {
     while (eventIDQueue.size() && eventIDQueue.top() == displayIndex) {
-        animationQueue.top()->apply(ui);
+        animationQueue.top()->apply(getCurrentUI());
         animationQueue.pop();
         eventIDQueue.pop();
     }
     displayIndex++;
+}
+
+template<typename TypeUI>
+TypeUI& AnimationManager<TypeUI>::getCurrentUI() {
+    return stateUI[stateIterator];
+}
+
+template <typename TypeUI>
+void AnimationManager<TypeUI>::previousState() {
+    if (stateIterator == 0)
+        return setCallbackSetPreviousState(false);
+    
+    // stateUI[stateIterator].copyPosition(stateUI[stateIterator - 1])
+    stateIterator--;
+    if (stateIterator == 0)
+        setCallbackSetPreviousState(false);
+    if (stateIterator + 2 == stateUI.size())
+        setCallbackSetNextState(true);
+}
+
+template <typename TypeUI>
+void AnimationManager<TypeUI>::nextState() {
+    if (stateIterator + 1 == stateUI.size())
+        return setCallbackSetNextState(false);
+    
+    // stateUI[stateIterator].copyPosition(stateUI[stateIterator + 1])
+    stateIterator++;
+    if (stateIterator == 1)
+        setCallbackSetPreviousState(true);
+    if (stateIterator + 1 == stateUI.size())
+        setCallbackSetNextState(false);
 }
